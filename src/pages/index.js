3c21f135fe71284730b004d34bc921fc4ad4ea08
Matheus Azzi <matheuslazzi@gmail.com>
2019-10-06 20:30:40 -0300

    List jobs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,43 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { graphql, Link } from 'gatsby'
 
-const IndexPage = () => <h1>Codeminer42 Careers</h1>
+const IndexPage = ({ data }) => {
+  const { jobs } = data
+
+  return (
+    <Fragment>
+      <h1>Codeminer42 Careers</h1>
+
+      {jobs.edges.length > 0 ? (
+        <ul>
+          {jobs.edges.map((edge) => {
+            const job = edge.node
+
+            return (
+              <li key={job.path}>
+                <Link to={job.path}>{job.title}</Link>
+              </li>
+            )
+          })}
+        </ul>
+      ) : (
+        <p>No positions to show.</p>
+      )}
+    </Fragment>
+  )
+}
+
+export const query = graphql`
+  query IndexQuery {
+    jobs: allJobsJson(sort: { order: ASC, fields: [path] }) {
+      edges {
+        node {
+          title
+          path
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
