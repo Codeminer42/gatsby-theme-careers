ef998f97396a913e26e8bfa6b05722cbb31fd4fe
Abner Soares Alves Junior <abner.alves@codeminer42.com>
2019-12-02 17:54:58 -0300

    Add Google Analytics Plugin

--- packages/gatsby-theme-careers/gatsby-config.js
+++ packages/gatsby-theme-careers/gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = ({
   title = 'Gatsby Theme Careers',
   spaceId,
   accessToken,
+  googleAnalyticsOptions,
 } = {}) => ({
   siteMetadata: {
     title,
@@ -16,6 +17,15 @@ module.exports = ({
         downloadLocal: true,
         spaceId,
         accessToken,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        //All options available https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/#how-to-use
+        ...googleAnalyticsOptions,
       },
     },
   ],
